1b862e24c630904be1e11c459b4583b3f4c27363
jiawenanan <54885276+jiawenanan@users.noreply.github.com>
2021-03-18 17:31:41 -0400

    Update removeCoveredIntervals.java

--- removeCoveredIntervals.java
+++ removeCoveredIntervals.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class removeCoveredIntervals {
-	 public int removeCoveredIntervals1(int[][] intervals) {
+	 public int removeCoveredIntervals(int[][] intervals) {
 	        if(intervals.length == 1){
 	            return 0;
 	        }
@@ -28,24 +28,5 @@ public class removeCoveredIntervals {
 	            }
 	    }
 	
-	public List<Integer> minAvailableDuration2(int[][] slots1, int[][] slots2, int duration) {
-        Arrays.sort(slots1, (a,b)->a[0]-b[0]); // sort increasing by start time
-        Arrays.sort(slots2, (a,b)->a[0]-b[0]); // sort increasing by start time
-
-        int i = 0, j = 0;
-        int n1 = slots1.length, n2 = slots2.length;
-        while (i < n1 && j < n2) {
-            int intersectStart = Math.max(slots1[i][0], slots2[j][0]);
-            int intersectEnd = Math.min(slots1[i][1], slots2[j][1]);
-
-            if (intersectStart + duration <= intersectEnd) // Found the result
-                return Arrays.asList(intersectStart, intersectStart + duration);
-            else if (slots1[i][1] < slots2[j][1])
-                i++;
-            else
-                j++;
-        }
-        return new ArrayList<>();
-    }
 }
 //add number
