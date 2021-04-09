428235e7ee4c1363df865e9879d61657453a5ffb
jiawenanan <54885276+jiawenanan@users.noreply.github.com>
2021-04-09 15:00:42 -0400

    Update flipMatchVoyage.java

--- flipMatchVoyage.java
+++ flipMatchVoyage.java
@@ -1,3 +1,40 @@
+class Solution {
+    List<Integer> res = new ArrayList<>();
+    public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
+        int count = 0;
+        List<Integer> neg = new ArrayList<>();
+        neg.add(-1);
+        return helper(count, voyage, root) == -1 ? neg : res;
+    }
+    
+    int helper(int count, int[] voyage, TreeNode root){
+        if(root == null){
+            return count;
+        }
+        if(root.val != voyage[count]){
+            return - 1;
+        }
+        int left = helper(count + 1, voyage, root.left);
+        if(left != - 1){
+            return helper(left, voyage, root.right);
+        }
+        res.add(root.val);
+        int right = helper(count + 1, voyage, root.right);
+        if(right != -1){
+            return helper(right, voyage, root.left);
+        }
+        return -1;
+    }
+}
+
+
+/*---------------------
+
+*/
+
+
+
+
 List<Integer> res = new ArrayList<>();
     boolean flag = true;
     public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
