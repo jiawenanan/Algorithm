d4127e9745a94debfc32cfdc420468673a930f7a
jiawenanan <54885276+jiawenanan@users.noreply.github.com>
2021-05-16 13:05:24 -0400

    minimumEffort

---
+++  minimumEffort.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimumEffort(int[][] tasks) {
+        int res = 0;
+        Arrays.sort(tasks, (a1, a2) -> (a1[1] - a1[0]) - (a2[1] - a2[0]));
+        for (int[] a : tasks) {
+            res = Math.max(res + a[0], a[1]);
+        }
+        return res;
+    }
+}
